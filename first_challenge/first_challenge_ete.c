cc94634568cc36430bb80ffac92cb3d120c6fac4
Etebom Ntuk <ntuketebom@gmail.com>
2022-10-15 08:47:39 -0700

    Ete's update

--- first_challenge/first_challenge_ete.c
+++ first_challenge/first_challenge_ete.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Return: 0 Success
+ */
 int main(void)
 {
+	/*variable declaration */
 	int age;
 
+	/*Acceptting data input using scanf() and assigning it to variable age */
 	printf("Check if a minor or an adult by entering an age:  ");
-	scanf("%d ", &age);
+	scanf("%d", &age);
 
 
+	/* The condition for sending output if underaged along with output to be printed*/
 	if ((age >= 0) && (age < 18))
 	{
 		printf("Minor \n");
 	}
+	/* The condition for capturing a real adult age along with its output*/
 	else if ((age >= 18) && (age <= 140))
 	{
 		printf("Adult \n");
 	}
+	/*The condition for unrealistic inputs along with its corresponding output*/
 	else
 	{
 		printf("Not a normal human age \n");
